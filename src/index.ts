29e84c5b9ab1ea7d96ee40c0246885836c41c3e6
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-05-14 16:12:07 +0100

    api:id re#4 can access one single questions

--- src/index.ts
+++ src/index.ts
@@ -43,3 +43,38 @@ app.get('/api', (req: Request, res: Response) => {
     });
 })
 
+interface Question {
+    id: number;
+    category: string;
+    difficulty: string;
+    question: string;
+    options: string[];
+    answer: string;
+    favourited: boolean;
+    timestamp: string;
+}
+
+
+app.get('/api/:id', (req:Request, res: Response) => {
+    const questionId = parseInt(req.params.id);
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        //this line parses the JSON data from the data variable into a Javascript Object.
+        //The JSON.parse function converts a JSON formatted string into a Javascrupt object. 
+        //after parsing, the resulting object is assign to the variable jsonDataObject
+        const jsonDataObject = JSON.parse(data)
+       // console.log(jsonDataObject)
+        //This line accesses th 'questions property of the jsonDataObject. 
+        //it assumes that the prsed JSON object has a property named 'questions',
+        //which contains an array of objects 
+        const questions: Question[] = jsonDataObject.questions
+        console.log(questions)
+        const question = questions.find((q: any) => q.id === questionId)
+        return res.json(question)
+    });
+})
+
